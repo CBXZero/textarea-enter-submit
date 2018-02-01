7b3e935d5fd988c017c9b25fe244d28de6f92196
Charlie <cbesalke@gmail.com>
2018-02-01 11:00:47 -0600

    Removing dist directory

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { TextAreaEnterDirective } from './text-area-enter.directive';
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var text_area_enter_directive_1 = require("./text-area-enter.directive");
-exports.TextAreaEnterDirective = text_area_enter_directive_1.TextAreaEnterDirective;
