0b9dfec7ef904ce7e21c52682a0e6f1537d19d91
Charlie Besalke <Charles.Besalke@vu.com>
2018-02-02 14:36:08 -0600

    Updating the text-area-enter module

--- src/text-area-enter.module.ts
+++ src/text-area-enter.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TextAreaEnterDirective } from './text-area-enter.directive';
 
 @NgModule({
-  declarations: [
-      TextAreaEnterDirective
-  ],
-  exports: [
-    TextAreaEnterDirective
-  ],
-  providers: [],
-  bootstrap: []
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        TextAreaEnterDirective
+    ],
+    exports: [
+        TextAreaEnterDirective
+    ],
+    providers: [],
+    bootstrap: []
 })
 export class TextAreaEnterModule { }
